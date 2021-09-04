70ea8c172ad39d1c509494a45037154705eefef7
AbelRose <15941999082@163.com>
2021-09-04 15:41:28 +0800

    [ADD] server-gateway

---
+++ server-gateway/src/main/java/com/matrix/gamll/gateway/ServerGatewayApplication.java
@@ -0,0 +1,16 @@
+package com.matrix.gamll.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: yihaosun
+ * @Date: 2021/9/4 15:35
+ */
+@SpringBootApplication
+public class ServerGatewayApplication {
+    public static void main(String[] args) {
+
+        SpringApplication.run(ServerGatewayApplication.class,args);
+    }
+}
