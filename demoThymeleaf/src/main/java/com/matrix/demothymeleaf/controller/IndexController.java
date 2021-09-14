f813452b8b9ff52b389595180dc1a7a10d3c6a8b
AbelRose <15941999082@163.com>
2021-09-14 20:49:44 +0800

    [CMT] thymeleaf labels

--- demoThymeleaf/src/main/java/com/matrix/demothymeleaf/controller/IndexController.java
+++ demoThymeleaf/src/main/java/com/matrix/demothymeleaf/controller/IndexController.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * @Author: yihaosun
@@ -13,8 +16,19 @@ import javax.servlet.http.HttpServletRequest;
 public class IndexController {
     // 编写控制器
     @GetMapping("index")
-    public String index(HttpServletRequest request) {
+    public String index(HttpServletRequest request, HttpSession session) {
         request.setAttribute("msg", "Hello...");
+
+        // 存一个集合数据
+        List<String> list = Arrays.asList("syh", "ly");
+        request.setAttribute("slist", list);
+
+        // 存一个年龄
+        request.setAttribute("age", 18);
+
+        // session存储
+        session.setAttribute("sname", "sunyihao");
+
         // 返回视图名称 默认的渲染路径是从resources/templates下面找视图名;
         return "index";
     }
