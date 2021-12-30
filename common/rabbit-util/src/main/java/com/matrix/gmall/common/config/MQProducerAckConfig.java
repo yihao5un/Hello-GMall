70a8f05e4ea42dfdaec8521f1acca53ad934581c
AbelRose <15941999082@163.com>
2021-12-30 09:26:17 +0800

    [CMT] rabbitMQ

--- common/rabbit-util/src/main/java/com/matrix/gmall/common/config/MQProducerAckConfig.java
+++ common/rabbit-util/src/main/java/com/matrix/gmall/common/config/MQProducerAckConfig.java
@@ -1,5 +1,8 @@
 package com.matrix.gmall.common.config;
 
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.connection.CorrelationData;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Component;
 
 /**
@@ -7,5 +10,33 @@ import org.springframework.stereotype.Component;
  * @Date: 2021/12/21 20:56
  */
 @Component
-public class MQProducerAckConfig {
+public class MQProducerAckConfig implements RabbitTemplate.ConfirmCallback, RabbitTemplate.ReturnCallback {
+    /**
+     * 消息成功发送到交换机上
+     *
+     * @param correlationData 消息的载体 带有Id标示的
+     * @param ack
+     * @param cause
+     */
+    @Override
+    public void confirm(CorrelationData correlationData, boolean ack, String cause) {
+        if (ack) {
+            // 消息发送到了交换机
+            System.out.println("消息发送成功!");
+        } else {
+            System.out.println("消息发送异常!");
+        }
+    }
+
+    /**
+     * @param message
+     * @param i
+     * @param s
+     * @param s1
+     * @param s2
+     */
+    @Override
+    public void returnedMessage(Message message, int i, String s, String s1, String s2) {
+
+    }
 }
